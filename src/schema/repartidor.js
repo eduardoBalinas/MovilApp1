12cc5df33181b3d36f1b2fdbc6b9118ed46c3954
eduardoBalinas <eduardo.balinas.lira@gmail.com>
2019-10-30 20:33:19 +0000

    Esta version tiene iniciado el schema de repartidor

---
+++ src/schema/repartidor.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const repartidorSchema = new Schema({
+
+});
+
+module.exports = mongoose.model('Repartidor', repartidorSchema);
